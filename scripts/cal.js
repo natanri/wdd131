4021404c94fa8b8ef8abf324aba9c39c5de57f59
natanri <riosjhonatan44@gmail.com>
2024-06-10 19:56:55 -0500

    Added features

--- scripts/cal.js
+++ scripts/cal.js
@@ -9,3 +9,7 @@ function displayResult(result) {
 
 calculate(2,3,displayResult) //the function callback is a distractor you should call the displayResult 
                             //to get the result
+
+const countries = ["Canada", "Chili", "Colombia", "Croatia", "Costa Rica"];
+const countriesLong = countries.filter((country) => country.length > 7);
+console.log(countriesLong)
